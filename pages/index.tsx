fd0903ed8c10b8539b8b2fcf9a26c70f4b34882d
KimJ Manansala <kimjmanansala@gmail.com>
2023-06-15 20:23:46 -0500

    [feat] creating form for ticket upload

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,42 +2,53 @@ import React from "react"
 import { GetStaticProps } from "next"
 import Layout from "../components/Layout"
 import Post, { PostProps } from "../components/Post"
+import prisma from "../lib/prisma";
 
 export const getStaticProps: GetStaticProps = async () => {
-  const feed = [
-    {
-      id: "1",
-      title: "Prisma is the perfect ORM for Next.js",
-      content: "[Prisma](https://github.com/prisma/prisma) and Next.js go _great_ together!",
-      published: false,
-      author: {
-        name: "Nikolas Burk",
-        email: "burk@prisma.io",
-      },
-    },
-  ]
-  return { 
-    props: { feed }, 
-    revalidate: 10 
-  }
-}
+  const user = await prisma.userType.findMany();
+  console.log('user', user);
+  return {
+    props: { user },
+    revalidate: 10,
+  };
+};
 
 type Props = {
-  feed: PostProps[]
+  user: PostProps[]
 }
 
 const Blog: React.FC<Props> = (props) => {
   return (
     <Layout>
       <div className="page">
-        <h1>Public Feed</h1>
-        <main>
-          {props.feed.map((post) => (
-            <div key={post.id} className="post">
-              <Post post={post} />
+        <div className="section">
+          <h1>Existing Tickets</h1>
+          <main>
+            Hello world
+          </main>
+        </div>
+        <div className="section">
+          <h1>Create a new ticket</h1>
+          <form style={{
+            display: "grid",
+            columnGap: 10,
+            rowGap: 10,
+          }}>
+            <div className="form_input">
+              <label>Name</label>
+              <input name="name" />
             </div>
-          ))}
-        </main>
+            <div className="form_input">
+              <label>Email</label>
+              <input name="email"/>
+            </div>
+            <div className="form_input">
+              <label>Description</label>
+              <textarea name="description"/>
+            </div>
+            <button style={{width: 'fit-content'}}>Submit!</button>
+          </form>
+        </div>
       </div>
       <style jsx>{`
         .post {
@@ -51,6 +62,22 @@ const Blog: React.FC<Props> = (props) => {
 
         .post + .post {
           margin-top: 2rem;
+        }
+        .form_input {
+          display: block;
+          justify-content: space-between;
+        }
+        .page {
+          max-width: 100vw;
+          margin: auto;
+        }
+        .form_input > label {
+          display: block;
+        }
+        .form_input > input, textarea {
+          display: block;
+          width: 100%;
+          padding-top: 2px;
         }
       `}</style>
     </Layout>
