2c541da253a47324f5cec8508df824c7a3c2803f
KimJ Manansala <kimjmanansala@gmail.com>
2023-06-16 01:23:38 -0500

    [misc] code clean up

---
+++ pages/admin_portal/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Layout from "../../components/Layout";
+
+const Index = () => {
+    return (
+        <Layout>
+            <div className="page">
+                Hello
+            </div>
+        </Layout>
+    );
+};
+
+export default Index;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,8 @@
 import React from "react"
-import { GetStaticProps } from "next"
+
 import Layout from "../components/Layout"
-import Post, { PostProps } from "../components/Post"
-import prisma from "../lib/prisma";
 
-export const getStaticProps: GetStaticProps = async () => {
-  const user = await prisma.userType.findMany();
-  console.log('user', user);
-  return {
-    props: { user },
-    revalidate: 10,
-  };
-};
-
-type Props = {
-  user: PostProps[]
-}
-
-const Blog: React.FC<Props> = (props) => {
+const Blog: React.FC<{}> = () => {
   return (
     <Layout>
       <div className="page">
