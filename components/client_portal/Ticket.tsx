bea1a3589397242d82c95337b7780bb64a5be1e4
KimJ Manansala <40606399+KimjManansala@users.noreply.github.com>
2023-06-19 18:12:32 -0500

    Code cleanup (#4)
    * [misc] code clean up
    
    * [feat] added preinstall
    
    * [feat] cleanup
    
    * [misc] fix deploy issue
    
    * [misc] small cleanup to get ready for admin page
    
    * [feat] Admin page able to view and update / responde to ticket
    
    * [feat] add refetch callback
    
    * [misc] styling clean up
    
    * [feat] updates to the readme
    
    * [misc] small styling update
    
    * [misc] styling update and added required

--- components/client_portal/Ticket.tsx
+++ components/client_portal/Ticket.tsx
@@ -1,34 +1,34 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {bold} from "kleur/colors";
 
-const Ticket = ({ticket}) => {
+const Ticket = ({ticket, ticketNumber}) => {
+    const [isExpand, setIsExpand] = useState<boolean>(false);
     return (
         <div>
-            <div className="flip-card">
-                <div className="flip-card-inner">
-                    <div className="flip-card-front">
-                        <p className="title">{ticket.name}</p>
-                        <p>Click for more info</p>
+            <div>
+                <div className="card">
+                    <div>
+                        <h3>Ticket #{ticketNumber}</h3>
+                        {!isExpand && (
+                        <button onClick={() => {setIsExpand(!isExpand)}}>Click for more info</button>
+                        )}
                     </div>
-                    <div className="flip-card-back">
-                        <p className="detail">Email: {ticket.name}</p>
-                        <p className="detail">Description: {ticket.description}</p>
-                        <p className="detail">Status: {ticket.status.status}</p>
-                    </div>
+                    {isExpand && (
+                        <div>
+                            <p className="detail">Name: {ticket.name}</p>
+                            <p className="detail">Email: {ticket.email}</p>
+                            <p className="detail">Description: {ticket.description}</p>
+                            <p className="detail">Status: {ticket.status.status}</p>
+                            <button onClick={() => {setIsExpand(!isExpand)}}>Minimize</button>
+                        </div>
+                    )}
                 </div>
             </div>
             <style jsx>{`
-              .flip-card {
-              padding: 5px;
-              background-color: transparent;
-              width: 190px;
-              height: 127px;
-              perspective: 1000px;
-              font-family: sans-serif;
-            }
-            
+
             .title {
               font-size: 1.5em;
-              // font-weight: 900;
+
               text-align: center;
               margin: 0;
             }
@@ -36,46 +36,16 @@ const Ticket = ({ticket}) => {
                 font-size: small;
                 text-align: left;
             }
-            
-            .flip-card-inner {
-              position: relative;
-              width: 100%;
-              height: 100%;
-              text-align: center;
-              transition: transform 0.8s;
-              transform-style: preserve-3d;
-            }
-            
-            .flip-card:hover .flip-card-inner {
-              transform: rotateY(180deg);
-            }
-            
-            .flip-card-front, .flip-card-back {
-              // box-shadow: 0 8px 14px 0 rgba(0,0,0,0.2);
-              position: absolute;
-              display: flex;
-              flex-direction: column;
-              justify-content: center;
-              width: 100%;
-              height: 100%;
-              -webkit-backface-visibility: hidden;
-              backface-visibility: hidden;
-              border: 1px solid grey;
-              // border-radius: 1rem;
-            }
-            
-            .flip-card-front {
-              background: linear-gradient(120deg, rgb(80, 50, 50, .2) 60%, rgb(50, 50, 50, .2) 88%,
-                 rgba(50, 50, 50, .5) 40%, rgba(148, 148, 148, 1) 48%);
-              color: black;
-            }
-            
-            .flip-card-back {
-              background: linear-gradient(120deg, rgb(80, 50, 50, .2) 60%, rgb(50, 50, 50, .2) 88%,
-                 rgba(50, 50, 50, .5) 40%, rgba(148, 148, 148, 1) 48%);
-              color: black;
-              transform: rotateY(180deg);
-              padding: 5px
+            .card {
+                flex: 0 0 200px;
+                margin: 10px;
+                border: 1px solid #ccc;
+                box-shadow: 2px 2px 6px 0px  rgba(0,0,0,0.3);
+                display: flex;
+                flex-direction: column;
+                max-width: 100%;
+                padding: 5px;
+                
             }
             `}</style>
         </div>
